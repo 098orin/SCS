745d86112223c2203f1468d11b6ea8ce7be4c8d0
098orin <mariosaakirisame1@gmail.com>
2025-01-31 18:10:37 +0900

    起動メッセージ簡略化

--- fun.py
+++ fun.py
@@ -11,7 +11,7 @@ try:
 except:
     os.system("pip install -U scratchattach")
     import scratchattach as scratch3
-print(os.system("pip show scratchattach"))
+    print(os.system("pip show scratchattach"))
 
 # global i2
 
