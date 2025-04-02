c772c8c77bf62c03101e1e17fcd59670b10214fd
098orin <mariosaakirisame1@gmail.com>
2025-04-02 20:34:12 +0900

    暗号化モジュール用の調整

---
+++ tools.py
@@ -0,0 +1,17 @@
+import importlib.util
+import subprocess
+import sys
+
+def install_if_not_exists(package_name: str):
+    spec = importlib.util.find_spec(package_name)
+    if spec is not None:
+        pass
+    else:
+        print(f"パッケージ '{package_name}' が見つかりません。インストールを開始します...")
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+            print(f"パッケージ '{package_name}' のインストールが完了しました。")
+        except subprocess.CalledProcessError:
+            print(f"パッケージ '{package_name}' のインストールに失敗しました。")
+        except Exception as e:
+            print(f"予期せぬエラーが発生しました: {e}")
